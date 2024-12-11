4f88a79f27667a89f74fcb5899130232ba121efd
Daniele Tomaselli <85880657+usedToBeTomas@users.noreply.github.com>
2024-12-11 10:12:04 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
+import os
 from setuptools import setup
+
+directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(directory, 'README.md'), encoding='utf-8') as f:
+  long_description = f.read()
 
 setup(
     name='hox',
-    version='1.0.3',
+    version='1.1.0',
     license='MIT',
     url = 'https://github.com/usedToBeTomas/hox',
     author='usedToBeTomas',
-    description='Lightweight nn library project.',
+    description='Lightweight neual network library project.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     packages=['hox'],
     python_requires='>=3.6',
     keywords = ['neural network', 'ml', 'ai', 'machine learning','vanilla','nn'],
